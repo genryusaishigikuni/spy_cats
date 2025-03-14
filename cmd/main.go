58ac02279c7b5e7e0f1ccd4b488b0866140dd03e
Tamerlan <tamerlanimanov188@gmail.com>
2025-03-14 08:44:40 +0400

    Almost finished

--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,31 @@
 package main
 
 import (
-	"log"
-
 	"github.com/genryusaishigikuni/spy_cats/config"
 	"github.com/genryusaishigikuni/spy_cats/pkg/database"
 	"github.com/genryusaishigikuni/spy_cats/pkg/router"
+	"log"
 )
 
 func main() {
-	// 1. Here I Load config
+	// Load configuration
 	cfg := config.Load()
 
-	// 2. Here I establish the connection with DB
+	// Connect to database
 	db, err := database.Connect(cfg.DB)
 	if err != nil {
 		log.Fatalf("Cannot connect DB: %v", err)
 	}
 
-	// Now I run migrations
+	// Run database migrations
 	if err := database.RunMigrations(db); err != nil {
 		log.Fatalf("Migration failed: %v", err)
 	}
 
-	// 4. Here I set up router
+	// Setup router
 	r := router.SetupRouter(db)
 
-	// 5. Finally, I start the server
+	// Start server
 	if err := r.Run(cfg.ServerPort); err != nil {
 		log.Fatalf("Server failed to start: %v", err)
 	}
